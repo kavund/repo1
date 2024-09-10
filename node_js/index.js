c21208120e2beb59e1ba2e683722029b70a7911c
kavun1 <steel4111@gmail.com>
2024-09-10 17:45:52 +0300

    export/import test

---
+++ node_js/index.js
@@ -0,0 +1,11 @@
+//const chalk = require('chalk')
+import chalk from 'chalk'
+
+//const {age} = require('./test.js')
+import testNumber, {print} from './test.js'
+
+console.log(testNumber)
+print('Hello JS')
+console.log(chalk.cyan('Hello Chalk'))
+
+//console.log(chalk.green('Hello JS'))
