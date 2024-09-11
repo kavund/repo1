b06438e0e434fe02c5699081cc17b0ac6a3cb7be
kavun1 <steel4111@gmail.com>
2024-09-11 15:59:04 +0300

    types

---
+++ js_basics/numbers.js
@@ -0,0 +1,32 @@
+// const ageString = '18'
+// console.log(ageString)
+// console.log(typeof ageString)
+
+// const age = Number(ageString)
+// const age = +ageString
+// console.log(age)
+// console.log(typeof age)
+
+// const stringValue = '123.4px'
+// console.log(Number(stringValue))
+// console.log(typeof Number(stringValue))
+// const pxValue = Number.parseFloat(stringValue)
+
+// console.log(pxValue)
+// console.log(typeof pxValue)
+
+// const result = 3 / '1'
+// console.log(isNaN(result))
+
+// const y = 0.1;
+// const x = 0.2;
+// const sum = x + y;
+// console.log(+sum.toFixed(2))
+
+// console.log(Math.pow(2,2))
+// console.log(Math.floor(Math.random() * 100))
+
+const firstText = 'Hello'
+const secondText = 'World'
+const resultText = firstText + secondText
+console.log(resultText)
---
+++ js_basics/strings.js
@@ -0,0 +1,34 @@
+// const singleQuote = '\'Hello\', "World"!'
+
+// const doubleQuote = "Hello, 'doublequote'"
+
+// const name = 'Alice'
+// const backtick = `${name} Hello, TikTok`
+
+
+
+// console.log(singleQuote)
+// console.log(doubleQuote)
+// console.log(backtick)
+
+// const text = 'JavaScriptsfdgjjjafqwhifhehsmcqmpiwrnihwihgtuufhsduvnoeM'
+// console.log(text.length)
+// console.log(text[1])
+// console.log(text[text.length - 1])
+
+// const firstText = 'Hello'
+// const secondText = 'World'
+// const resultText = firstText + ' ' + secondText
+// const resultText2 = `${firstText} ${secondText}`
+// console.log(resultText)
+// console.log(resultText2)
+
+let meme = 'Alice';
+console.log(meme.length)
+
+const name2 = meme.repeat(2)
+console.log(name2)
+
+console.log(meme.toUpperCase())
+console.log(meme.padStart(10, "ha"))
+console.log(meme.includes("a"))
