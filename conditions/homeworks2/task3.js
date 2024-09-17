7b8b55bdcaaa3a21adb7d677e879de61e7386c01
kavun1 <steel4111@gmail.com>
2024-09-17 12:06:28 +0300

    test

--- conditions/homeworks2/task3.js
+++ conditions/homeworks2/task3.js
@@ -1,5 +1,4 @@
-let number = 2
-
+let number = 11
 for (let i = 1; i <= 10; i++) {
     console.log(`${number} * ${i} = ${number * i}`)
 }
--- conditions/homeworks2/task4.js
+++ conditions/homeworks2/task4.js
@@ -1,4 +1,4 @@
-let number = 5
+let number = 6
 let i = 1
 while (i <= 10){
     console.log(`${number} * ${i} = ${number * i}`)
